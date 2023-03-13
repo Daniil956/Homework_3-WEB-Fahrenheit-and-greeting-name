1f42f22e15ccb5a56750d8253b60ea59ccdc38a5
Даниил <geimer_cok@mail.ru>
2023-03-13 19:43:17 +0300

    Workover

---
+++ 1.js
@@ -0,0 +1,8 @@
+let Fahrenheit = +prompt("Enter degress Celsius");
+
+alert(`Celsius: ${Fahrenheit}, Fahrenheit: ${trancletion(Fahrenheit)}.`);
+
+function trancletion(a) {
+    Celsius = (9/5)* Fahrenheit + 32;
+    return Celsius.toFixed(2)
+}
---
+++ 2.js
@@ -0,0 +1,7 @@
+let nickname = prompt("Enter name)");
+
+function greeting (name) {
+    console.log(`Hello ${name}`);
+}
+
+greeting(nickname);
